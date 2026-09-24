e54e1ba73e8295f956872425a6b13ad98cae97d8
netfiller-droid <confused@freenet.de>
2026-09-24 15:00:35 +0200

    Aktualisieren von server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ setInterval(() => {
 
 const app = express();
 let currentPort = Number(process.env.PORT || 7000);
-const ADDON_VERSION = '1.8.4';
+const ADDON_VERSION = '1.8.4.1';
 const DEFAULT_ADDON_NAME = 'UsenetStreamer';
 let serverInstance = null;
 const SERVER_HOST = '0.0.0.0';
